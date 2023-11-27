badb70ceca274ca20ddeaeef5532a997b052c6dd
Chin-I Feng <c.feng@outlook.de>
2023-11-27 14:31:29 +0100

    Added Linear Regression example with diabetes dataset

---
+++ diabetes_linear_regression.py
@@ -0,0 +1,20 @@
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+alldata = datasets.load_diabetes()
+data_X = alldata.data  # Features
+data_y = alldata.target  # Labels
+
+model = LinearRegression()
+model.fit(data_X, data_y)  # Train the model with the dataset
+
+# print("Predicted Labels:")
+# print(model.predict(data_X[:3, :]))
+#
+# print("Actual Labels:")
+# print(data_y[:3])
+
+X, y = datasets.make_regression(n_samples=150, n_features=1, n_targets=1, noise=15)
+plt.scatter(X, y)
+plt.show()
